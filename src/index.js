29ef4f4e854ef4214e9c248dfa0573ed07d63a1c
Elkin <elkilint@gmail.com>
2020-12-10 22:57:34 -0500

    version

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+import {Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+import './styles.css';
+export const todoList = new TodoList();
+todoList.todos.forEach(todo => crearTodoHtml(todo ));
+
+
+
+//const tarea = new Todo('Viajar');
+
+//todoList.nuevoTodo (tarea);
+
+//console.log(todoList);
+//crearTodoHtml(tarea);
